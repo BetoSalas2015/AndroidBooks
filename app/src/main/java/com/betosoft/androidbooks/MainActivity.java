50d8586c60abb4b84fa298487238042b312041ee
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2016-02-29 19:49:26 -0600

    Primera Carga

---
+++ app/src/main/java/com/betosoft/androidbooks/MainActivity.java
@@ -0,0 +1,52 @@
+package com.betosoft.androidbooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    RadioButton rdbAndroid6, rdbLearn, rdbEssential, rdbHeads;
+    TextView txtDescrip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rdbAndroid6 = (RadioButton) findViewById(R.id.rdbAndroid6);
+        rdbHeads = (RadioButton) findViewById(R.id.rdbHeads);
+        rdbLearn = (RadioButton) findViewById(R.id.rdbLearn);
+        rdbEssential = (RadioButton) findViewById(R.id.rdbEsential);
+        txtDescrip = (TextView) findViewById(R.id.txtDescrip);
+
+        rdbHeads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtDescrip.setText(R.string.android6Desc);
+            }
+        });
+        rdbLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtDescrip.setText(R.string.headsDEscrip);
+            }
+        });
+        rdbEssential.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtDescrip.setText(R.string.introDesc);
+            }
+        });
+        rdbAndroid6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtDescrip.setText(R.string.android6Desc);
+            }
+        });
+    }
+
+
+
+
+}
