8587e4839523f6bcbe14dff741a611ec2e46c14d
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2016-03-01 19:48:37 -0600

    Modificación Fragments

---
+++ app/src/main/java/com/betosoft/androidbooks/BookListFragment.java
@@ -0,0 +1,22 @@
+package com.betosoft.androidbooks;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Roberto Salazar on 29/02/2016.
+ *
+ * @version 1.0
+ */
+public class BookListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_book_list, container, false);
+
+    }
+}
